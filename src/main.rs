733e7860c488fe7a970dd323b5a8ea64148ab2b3
sandarau <alexandramaari@gmail.com>
2026-06-09 14:01:50 +0300

    we need to use the variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod hal;
+mod encoder;
+mod motor;
+mod pid;
+mod mapping;
+mod ultrasonic;
+mod motion;
+
 fn main() {
     println!("micromouse no_std control library: see src/lib.rs modules");
 }
